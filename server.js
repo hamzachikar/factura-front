d68adbd90dd4a40f199106e2163a7327cec0f0af
hamzachikar <chikar.h@outlook.fr>
2020-10-31 15:10:45 +0100

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-app.use(express.static('./dist/angular-heroku'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
-
+app.use(express.static(__dirname + '/dist/gestion-Facture-front'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/gestion-Facture-front/index.html'));});
 app.listen(process.env.PORT || 8080);
